262adcafc70d09672ae594756e9723e2766e58d9
DeFrakt <syaussi@gmail.com>
2019-09-22 16:31:00 -0700

    CardPile Update

--- index.js
+++ index.js
@@ -176,7 +176,7 @@ export default class RocketToTheTop extends Component {
             return cardPileArray;
         }
         // else if check for win?
-        //check for win
+      
     }
 
     render(){
